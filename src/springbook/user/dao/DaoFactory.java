1f24f57bcdafabe9456a4a6154f45f8f2f7bf00d
chihoon kim <kimddochi@gmail.com>
2011-08-16 20:29:44 +0900

    1.5 ~ 1장 끝.

--- src/springbook/user/dao/DaoFactory.java
+++ src/springbook/user/dao/DaoFactory.java
@@ -1,14 +1,31 @@
 package springbook.user.dao;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 
 @Configuration
 public class DaoFactory {
+
+	@Bean
+	public DataSource dataSource(){
+		SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+		
+		dataSource.setDriverClass(org.hsqldb.jdbcDriver.class);
+		dataSource.setUrl("jdbc:hsqldb:hsql://localhost/mydb");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		
+		return dataSource;
+	}
 	
 	@Bean
 	public UserDao userDao(){
-		return new UserDao(connectionMaker());
+		UserDao userDao = new UserDao();
+		userDao.setDataSource(dataSource());
+		return userDao;
 	}
 
 	@Bean
--- test/dao/UserDaoTests.java
+++ test/dao/UserDaoTests.java
@@ -4,6 +4,7 @@ import java.sql.SQLException;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import springbook.user.dao.DaoFactory;
 import springbook.user.dao.UserDao;
@@ -14,7 +15,7 @@ public class UserDaoTests {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException{
 		
 		ApplicationContext context =
-			new AnnotationConfigApplicationContext(DaoFactory.class);
+			new GenericXmlApplicationContext("applicationContext.xml");
 		
 		UserDao dao = context.getBean("userDao", UserDao.class);
 		

1ed752de87175cc9af0761f0ec7cbb1b8f2e6f98
chihoon kim <kimddochi@gmail.com>
2011-08-18 11:16:15 +0900

    3.4.2 코드를 이용하는 수동 DI JdbcContext클래스는 UserDao클래스에서 사용될 JDBC컨텍스트를 제공하는 서비스 오브젝트이므로 UserDao의 수정자 메소드에서 수동으로 DI를 해줄 수 있다. 단점 : 싱글톤으로 관리되는 것은 불가능해진다. 장점 : DI하는 방법이 외부에 드러나지 않는다.

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -18,10 +18,11 @@ public class UserDao {
 	private JdbcContext jdbcContext;
 	
 	public void setDataSource(DataSource dataSource) {
+		
+		this.jdbcContext = new JdbcContext();
+		this.jdbcContext.setDataSource(dataSource);
+		
 		this.dataSource = dataSource;
-	}
-	public void setJdbcContext(JdbcContext jdbcContext) {
-		this.jdbcContext = jdbcContext;
 	}
 	
 	public void deleteAll() throws SQLException{

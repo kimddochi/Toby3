7c35ab2fd1a45412b89e3012b3ca7d6242cd4176
chihoon kim <kimddochi@gmail.com>
2011-08-22 03:25:50 +0900

    3장 끝 3.5.3 템플릿/콜백의 응용은 생략함.

--- src/springbook/user/dao/UserDao.java
+++ src/springbook/user/dao/UserDao.java
@@ -1,98 +1,56 @@
 package springbook.user.dao;
 
-import java.sql.Connection;
-
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 import javax.sql.DataSource;
 
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import springbook.user.domain.User;
 
 public class UserDao {
 	
-	private DataSource dataSource;
-	private JdbcContext jdbcContext;
+	private JdbcTemplate jdbcTemplate;
 	
 	public void setDataSource(DataSource dataSource) {
-		
-		this.jdbcContext = new JdbcContext();
-		this.jdbcContext.setDataSource(dataSource);
-		
-		this.dataSource = dataSource;
+		this.jdbcTemplate = new JdbcTemplate(dataSource);
 	}
 	
-	
-	public void deleteAll() throws SQLException{
-		this.jdbcContext.executeSql("delete from users");
-	}
-	
-//	public void add(final User user) throws ClassNotFoundException, SQLException{
-//		this.jdbcContext.executeSql("insert into users(id, name, password) values(?, ?, ?)", user.getId());
-//	}
-
-	public void add(final User user) throws ClassNotFoundException, SQLException{
-			
-		this.jdbcContext.workWithStatementStrategy(
-				new StatementStrategy() {
-					@Override
-					public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-						PreparedStatement ps = c.prepareStatement("insert into users(id, name, password) values(?, ?, ?)");
-						ps.setString(1, user.getId());
-						ps.setString(2, user.getName());
-						ps.setString(3, user.getPassword());
-						return ps;
-					}
-				}
-		);
-	}
-
-	public int getCount() throws SQLException{
-		Connection c = null;
-		PreparedStatement ps = null;
-		ResultSet rs = null;
-		try {
-			c = dataSource.getConnection();
-			ps = c.prepareStatement("select count(*) from users");
-			rs = ps.executeQuery();
-			rs.next();
-			return rs.getInt(1);
-		} catch (SQLException e) {
-			throw e;
-		}finally{
-			if(rs!=null) try{ rs.close(); }catch(SQLException e){}
-			if(ps!=null) try{ ps.close(); }catch(SQLException e){}
-			if(c!=null)  try{  c.close(); }catch(SQLException e){}
-		}
-	}
-	
-	public User get(String id) throws ClassNotFoundException, SQLException{
-		Connection c = dataSource.getConnection();
-		
-		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
-		ps.setString(1, id);
-		
-		ResultSet rs = ps.executeQuery();
-		
-		User user = null;
-		if(rs.next()){
-			user = new User();
+	private RowMapper<User> userMapper = new RowMapper<User>() {
+		@Override
+		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+			User user = new User();
 			user.setId(rs.getString("id"));
 			user.setName(rs.getString("name"));
 			user.setPassword(rs.getString("password"));
+			return user;
 		}
-		
-		rs.close();
-		ps.close();
-		c.close();
-		
-		if(user == null) throw new EmptyResultDataAccessException(1);
-		
-		return user;
+	};
+
+	public void deleteAll(){
+		this.jdbcTemplate.update("delete from users");
+	}
+
+	public void add(final User user){
+		this.jdbcTemplate.update("insert into users(id, name, password) values(?, ?, ?)",
+				user.getId(),
+				user.getName(),
+				user.getPassword());
+	}
+
+	public int getCount() throws SQLException{
+		return this.jdbcTemplate.queryForInt("select count(*) from users");
+	}
+	
+	public User get(String id){
+		return this.jdbcTemplate.queryForObject("select * from users where id = ?", new Object[] {id}, this.userMapper);
+	}
+
+	public List<User> getAll() {
+		return this.jdbcTemplate.query("select * from users", this.userMapper);
 	}
 	
 }
--- test/dao/UserDaoTests.java
+++ test/dao/UserDaoTests.java
@@ -1,9 +1,10 @@
 package dao;
 
-import static org.hamcrest.CoreMatchers.*;
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
-import org.aspectj.lang.annotation.Before;
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -87,5 +88,37 @@ public class UserDaoTests {
 		
 		dao.get("unknown_id");
 		
+	}
+	
+	@Test
+	public void getAll() throws Exception {
+		dao.deleteAll();
+		
+		dao.add(user1);
+		List<User> users1 = dao.getAll();
+		assertThat(users1.size(), is(1));
+		checkSameUser(user1, users1.get(0));
+		
+		dao.add(user2);
+		List<User> users2 = dao.getAll();
+		assertThat(users2.size(), is(2));
+		checkSameUser(user1, users2.get(0));
+		checkSameUser(user2, users2.get(1));
+		
+		dao.add(user3);
+		List<User> users3 = dao.getAll();
+		assertThat(users3.size(), is(3));
+		checkSameUser(user3, users3.get(0));
+		checkSameUser(user1, users3.get(1));
+		checkSameUser(user2, users3.get(2));
+		
+		List<User> users0 = dao.getAll();
+		assertThat(users0.size(), is(0));
+	}
+
+	private void checkSameUser(User user1, User user2) {
+		assertThat(user1.getId(), is(user2.getId()));
+		assertThat(user1.getName(), is(user2.getName()));
+		assertThat(user1.getPassword(), is(user2.getPassword()));
 	}
 }
